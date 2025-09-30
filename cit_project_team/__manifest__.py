e06a5cfbfbb3dfec57b442260e8b9bbf0f2acb81
Sirajudin Badi <sirubadi79@gmail.com>
2025-09-30 14:21:10 +0530

    [ADD]-project_team:project_team_member model

---
+++ cit_project_team/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#                                                                            #
+# Part of Caret IT Solutions Pvt. Ltd. (Website: www.caretit.com).           #
+# See LICENSE file for full copyright and licensing details.                 #
+#                                                                            #
+##############################################################################
+{
+    'name': 'Project Team',
+    'summary': 'Project management module covering teams and member operations',
+    'description': '''Aims to manage the operations of team and member for project.''',
+    'version': '19.0.1.0',
+    'author': 'Caret It Solutions PVT. LTD.',
+    'category': 'Project',
+    'website': 'https://caretit.com',
+    'depends': ["base", 'project'],
+    "installable" : True,
+    "application" : True,
+    'data': [],
+    'license': 'LGPL-3',
+    'images': ['static/description/icon.png'],
+}
+
---
+++ cit_project_team/models/project_team_member.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#                                                                            #
+# Part of Caret IT Solutions Pvt. Ltd. (Website: www.caretit.com).           #
+# See LICENSE file for full copyright and licensing details.                 #
+#                                                                            #
+##############################################################################
+from odoo import models, fields, api
+
+
+class TeamMember(models.Model):
+    _name = "project.team.member"
+    _description = "Project Team Member"
+
+    name = fields.Char("Name", required=True, help="Name of the new team member")
+    house_no = fields.Char("House No.", help="House No. for member's address")
+    street = fields.Char("Street", help="Street Name for address")
+    street2 = fields.Char("Street 2", help="Street 2 Name for address")
+    country_id = fields.Many2one("res.country", "Country", required=True, help="Country of a member")
+    state_id = fields.Many2one("res.country.state", "State", required=True, help="State of a member")
+    zip = fields.Char("Zip", required=True, help="Address ZIP/Postal Code")
+    mobile = fields.Char("Mobile", required=True, help="Personal Mobile number of a member")
+    user_id = fields.Many2one(
+        "res.users",
+        required=True,
+        help="User associated with this member",
+        context = {"search_default_employee" : 1}
+    )
+    email = fields.Char(related="user_id.email", required=True, help="Email ID of a member fetched from user")
+    gender = fields.Selection(
+        selection=[("male", "Male"), ("female", "Female"), ("other", "Other")],
+        required=True,
+        help="Gender of a member"
+    )
+    birth_date = fields.Date("Birth Date", help="Member's date of birth")
+    user_image = fields.Binary("Image", help="Upload avatar image of a member")
+    bio_data = fields.Html("Bio Data", help="Summarise the new member's bio")
+    active = fields.Boolean("Active", default=True, help="Member status")
+    timesheet_ids = fields.One2many(
+        "account.analytic.line",
+        "user_id",
+        "Timesheets",
+        help="Timesheets entries of a user"
+    )
+
